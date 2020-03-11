1d407ca2053a67d0e86a14311fe38d05ceaa4b37
pythonpete32 <pythonpete32@gmail.com>
2020-03-11 12:37:46 +0000

    sc

--- index.js
+++ index.js
@@ -1,52 +1,85 @@
 #!/usr/bin/env node
 
-const Listr = require("listr");
-const f = require("./functions");
+const chalk = require('chalk')
+const clear = require('clear')
+const figlet = require('figlet')
+const Configstore = require('configstore')
+const inquirer = require('./lib/inquirer')
+
 //
-// Flow:
-//    - get info from json file : token, url, DAO addresses
-//    - `yarn backend`
-//    - await `node bin/sourcecred.js discourse https://discourse.sourcecred.io`
-//    - await `node bin/sourcecred.js scores discourse.sourcecred.io`
-//    - format grain
-//    - Save grain to json file
-//    - display grain
-//    - Ask to mint
-//    - Mint
-//
+const processing = require('./lib/processing');
+const sc = require('./lib/cred');
 
-process.env.SOURCECRED_GITHUB_TOKEN =
-  "";
 
-//const config = require("../scores.json");
+// data store TODO: persist,  Data from add DAO. 
+const pkg = require('./package.json')
+const daoConf = new Configstore(pkg.name)
+daoConf.set('tokenManager', '0xdddf3dc0c04cb25b585ed042d21b58a0401cf3d1')
+daoConf.set('voting', '0x85e699f4a1726d5bf6f409d2bd63213c4afd85a0')
+daoConf.set('dao', '0xD08FF71fba48d4f08Ccd27d7538b9ea365730A310x0f1449527c458EDee87d19C39CA4f0326818aA46')
+daoConf.set('network', 'rinkeby')
 
+// banner
+const banner = () => {
+    clear();
+    console.log(
+        chalk.blueBright(
+            figlet.textSync('Power Tools', {
+                horizontalLayout: 'full'
+            })
+        )
+    )
+    console.log(
+        chalk.blue.bold('For Aragon Power Users')
+    )
+}
+
+
+// run program
 const run = async () => {
-  const tasks = new Listr([{
-      title: "Run Backend",
-      task: async () => {
-        await f.startBackend();
-      },
-    },
-    {
-      title: "Run SourceCred",
-      task: async () => {
-        await f.runSC();
-      },
-    },
-    {
-      title: "Calc Grain",
-      task: async () => {
-        console.log(await f.calcCred());
-      },
-    },
-    {
-      title: "saveCSV",
-      task: async () => {
-        await f.processCSV();
-      },
-    },
-  ]);
+    banner()
 
-  tasks.run();
-};
-run();
+    // login logic
+    const newDAO = await inquirer.login()
+    //console.log(newDAO)
+    if (newDAO) {
+        const dao = await inquirer.askDaoAddresses()
+        console.log(dao)
+        console.log(daoConf)
+    }
+
+    // main menu
+    let c = await inquirer.menu()
+    if (c.command == 'mint') {
+        const mints = await inquirer.getMints();
+        await inquirer.confirmMints(mints)
+        await processing.saveTxConfig(mints)
+        await processing.submitTx()
+    }
+    if (c.command == 'payments') {
+        console.log('payments')
+    }
+    if (c.command == 'sourcecred') {
+        const mode = await sc.mode()
+        await sc.startBackend()
+        console.log(mode.discourse)
+        await sc.runSC(mode.discourse)
+        await sc.calcCred()
+        const cred = await sc.processCSV()
+        await inquirer.confirmMints(cred)
+        await sc.resolveAddresses(cred)
+        const tx = await processing.saveTxConfig()
+        await processing.submitTx(tx)
+        console.log(mode)
+    }
+    if (c.command == 'swap') {
+        console.log('swap')
+    }
+    if (c.command == 'permissions') {
+        console.log('permissions')
+    }
+    if (c.command == 'quit') {
+        console.log('quit')
+    }
+}
+run()
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Aracred CLI
 
 Aracred CLI runs SourceCred on a Discorse server, and mints the cred on chain in an Aragon DAO
@@ -31,3 +32,35 @@ npm link
 ```
 
 navigate back to the SourceCred folder and type `aracli` into the terminal
+=======
+# Power tools
+
+> For Aragon Power users
+> These are a collection of power tools for Aragon power users.
+
+### Problem: CLI Too Difficult For Most Power Users
+
+-   Cli is a very powerfull tool.
+-   However it requires a degree of technical sofistication
+-   most of its potential users [.......]
+
+### Solution:
+
+### Result:
+
+npm i
+node index.js
+
+```
+## behaviour
+- [x] on start up it loads the last DAO if there was one
+- [x] asks user to use this dao or new
+  - [x] if new enter the add dao flow
+- [x] drops into tool selection, displys all 4 tools with information
+  - [x] drops into tool flow
+  - [x] confirm submittion
+- [x] submit transaction
+- [ ] add sourcecred
+
+```
+>>>>>>> start SC logic
